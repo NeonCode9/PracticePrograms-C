3224afbcfdac7d68d69d326db5d01ca0a2242349
SG <116645306+NeonCode9@users.noreply.github.com>
2022-10-28 12:42:20 +0530

    Updated condition

--- MarksGrading-if-else.c
+++ MarksGrading-if-else.c
@@ -8,19 +8,23 @@ int main()
 	scanf("%d", &marks);
 	if (marks <= 30)
 	{
-		printf("Grade is 'C'");
+		printf("Grade is 'C'\n");
 	}
 	else if (marks > 30 && marks < 70)
 	{
-		printf("Grade is 'B'");
+		printf("Grade is 'B'\n");
 	}
 	else if (marks > 70 && marks < 90)
 	{
-		printf("Grade is 'A'");
+		printf("Grade is 'A'\n");
 	}
-	else if (marks >= 90 && marks < 100)
+	else if (marks >= 90 && marks <= 100)
 	{
-		printf("Grade is 'A+'");
+		printf("Grade is 'A+'\n");
+	}
+	else if (marks > 100)
+	{
+		printf("Invalid Input\n");
 	}
 	return 0;
 }
